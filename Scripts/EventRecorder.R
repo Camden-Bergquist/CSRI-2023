3e3f96d67d0a2001cad3e48dfe82de11607b868a
Camden Bergquist <camdentbergquist@gmail.com>
2023-06-23 13:27:59 -0500

    Basic multi-analysis grade functionality.
    Tweaked comments on eventRecorder.

--- Scripts/EventRecorder.R
+++ Scripts/EventRecorder.R
@@ -1,4 +1,4 @@
-# Initialize an empty data frame
+# Initialize an empty data frame to record information.
 df <- data.frame(
   pin = character(),
   problem = character(),
@@ -6,16 +6,19 @@ df <- data.frame(
   answer = character(), 
   correct = logical(),
   question_num = character(),
-  stringsAsFactors = FALSE  # Prevent strings being converted to factors
+  stringsAsFactors = FALSE  # Prevent strings being converted to factors.
 )
 
-# Initialize pin to NA outside the function
+# The variable 'pin' is initialized to NA. 
+# It's used to handle cases where the tutorial event occurs before the user is authenticated,
+# or else, there's no authentication question in the first place.
 pin <- NA
 
-# Function to record tutorial events
+# This function is designed to handle, process, and store incoming learnr tutorial events. 
 eventRecorder <- function(tutorial_id, tutorial_version, event, data, user_id) {
   
-  # Authenticate with Google Sheets
+  # This block authenticates with Google Sheets and Google Drive using a JSON token file.
+  # The authentication allows the function to interact with Google services, such as storing data in Google Sheets.
   gs4_auth(
     path = "~/CSRI-2023/Auth/csri-database-token.json",
     scopes = c(
@@ -24,40 +27,40 @@ eventRecorder <- function(tutorial_id, tutorial_version, event, data, user_id) {
     )
   )
   
-  # Authenticate with Google Drive
   drive_auth(path = "~/CSRI-2023/Auth/csri-database-token.json")
   
-  # Check if event is exercise_submission or question_submission
+  # This 'if' condition filters incoming events. The goal is to only process 'exercise_submission' and 'question_submission' events,
+  # as these contain meaningful user data. All other event types aren't useful for the current purposes and are ignored.
   if (event %in% c("exercise_submission", "question_submission")) {
     
-    # Check if question contains [Authentication]
+    # The following condition checks for the authentication question. The user's submitted pin
+    # is saved for later use to identify which user submitted the upcoming tutorial events.
+    # For each non-authentication and non-submission question, data is extracted and stored.
     if (grepl("\\[Authentication\\]", data$question)) {
-      # Store the answer to the authentication question as the pin
-      pin <<- data$answer
-    } else if (data$question != "Submit Assignment?") {  # Ignore 'Submit Assignment?' question
+      pin <- data$answer
+    } else if (data$question != "Submit Assignment?") {  # Ignore 'Submit Assignment?' question since it carries no important info.
       
-      # Extract question number from the question
+      # Extract question number and points from the regex in the question title.
       question_num <- ifelse(grepl("\\[Question \\d+\\]", data$question), 
                              gsub(".*\\[Question (\\d+)\\].*", "\\1", data$question), NA)
       
-      # Extract points from the question
       points <- ifelse(grepl("\\[Points: \\d+\\]", data$question), 
                        as.integer(gsub(".*\\[Points: (\\d+)\\].*", "\\1", data$question)), 1)
       
-      # Check if there's already a row with the same pin and question_num
+      # To account for multiple attempts, check if there's already a row with a given question's identifiers.
       previous_rows <- df[df$pin == pin & gsub("\\..*", "", df$question_num) == paste0("Q", question_num), ]
       if (nrow(previous_rows) > 0) {
         attempt_num <- max(as.numeric(gsub(".*\\.(\\d+)$", "\\1", previous_rows$question_num))) + 1
-        # Adjust points for retries
+        # For each attempt past the first, deduct half a point.
         points <- points - 0.5 * (attempt_num - 1)
       } else {
         attempt_num <- 1
       }
       
-      # Extract problem without [Question X] and [Points: X]
+      # Extract problem query without regex identifiers.
       problem <- gsub("\\[Question \\d+\\]|\\[Points: \\d+\\]", "", data$question)
       
-      # Create new row with event data
+      # Creating and appending new row with the event data to add to our data frame.
       new_row <- data.frame(
         pin = pin,
         problem = problem,
@@ -67,34 +70,31 @@ eventRecorder <- function(tutorial_id, tutorial_version, event, data, user_id) {
         question_num = paste0("Q", question_num, ".", attempt_num),
         stringsAsFactors = FALSE
       )
-      
-      # Append new row to global data frame
-      df <<- rbind(df, new_row)
+    
+      df <- rbind(df, new_row)
     }
     
-    # If it's the final question, reshape and write data frame to Google Sheets
+    # In reaction to the submission question, the function transforms the data and writes it to a Google Sheet.
     if(data$question == "Submit Assignment?") {
       
-      # Reshape data frame to long format
+      # Reshape/pivot.
       df_long <- df %>% 
         gather(key = "key", value = "value", -pin, -question_num)
       
-      # Pivot the data frame back to wide format
       df_wide <- df_long %>% 
         pivot_wider(names_from = c("question_num", "key"), names_sep = "_", values_from = "value")
       
-      # Generate the column order
+      # Generate column order, reorder.
       unique_questions <- sort(unique(df$question_num))
       col_order <- c("pin", unlist(lapply(unique_questions, function(i) paste0(i, c("_problem", "_answer", "_correct", "_points")))))
       
-      # Reorder the columns
       df_wide <- df_wide[, col_order]
       
-      # Create a new Google Sheet and write the data frame to it
+      # Create a new Google Sheet and writing the reshaped data frame to it.
+      # Then, share the new Google Sheet with a given user.
       ss <- gs4_create(name = "logfile")
       sheet_write(df_wide, ss = ss, sheet = "Sheet1")
       
-      # Share the new Google Sheet with a user
       drive_share(
         ss,
         role = "writer",
--- Shiny Apps/multiAnalysis/app.R
+++ Shiny Apps/multiAnalysis/app.R
@@ -27,6 +27,7 @@ ui <- fluidPage(
 )
 
 server <- function(input, output) {
+  
   data <- eventReactive(input$run_analysis, {
     homework_dir <- "./Assignments"
     
@@ -36,136 +37,51 @@ server <- function(input, output) {
       return(NULL)
     }
     
-    data <- filePaths %>%
+    rawData <- filePaths %>%
       set_names(nm = basename(.)) %>%
       map_df(~read_csv(.x, guess_max = 1e6), .id = 'assignment') %>%
       mutate(assignment = str_remove(assignment, ".csv")) %>%
       type_convert() # convert column types
     
-    # Convert all answer columns to the same type
-    names <- names(data)
-    answer_cols <- grep("^Q.*_answer$", names, value = TRUE)
-    data[answer_cols] <- lapply(data[answer_cols], function(col) as.character(col))
-    data
+    # Change points to 0 where corresponding _correct column is not TRUE
+    pointsColumns <- grep("_points", names(rawData), value = TRUE)
+    correctColumns <- sub("_points", "_correct", pointsColumns)
+    for(i in seq_along(pointsColumns)) {
+      rawData <- rawData %>%
+        mutate_at(vars(pointsColumns[i]), ~ifelse(get(correctColumns[i]) == TRUE, ., 0))
+    }
+    
+    dataPoints <- rawData %>%
+      select(c("assignment", "pin", pointsColumns)) %>%
+      mutate(total_points = rowSums(select(., -c("assignment", "pin")), na.rm = TRUE)) %>%
+      select("assignment", "pin", "total_points")
+    
+    # Create a new data frame with each pin as a column
+    newData <- dataPoints %>%
+      pivot_wider(names_from = pin, values_from = total_points)
+    
+    # Get first attempts columns
+    firstAttemptCols <- grep("Q.*\\.1_points", names(rawData), value = TRUE)
+    
+    # Get the max sum of each first attempt of each question for each user
+    userMaxSum <- rawData %>%
+      group_by(assignment, pin) %>%
+      summarize(across(all_of(firstAttemptCols), sum, na.rm = TRUE), .groups = "drop") %>%
+      mutate(total = rowSums(across(where(is.numeric)), na.rm = TRUE)) %>%
+      group_by(assignment) %>%
+      summarize(max_total = max(total, na.rm = TRUE), .groups = "drop")
+    
+    # Add the max_total column
+    newData <- left_join(newData, userMaxSum, by = "assignment")
+    
+    return(newData)
   })
   
-  analysis <- eventReactive(input$run_analysis, {
-    df <- data()
-    
-    problems <- df %>%
-      select(user_id = pin, starts_with("Q") & ends_with("_problem")) %>%
-      pivot_longer(-user_id, names_to = "question", values_to = "problem", names_pattern = "(Q\\d+\\.\\d+)_problem")
-    
-    answers <- df %>%
-      select(user_id = pin, starts_with("Q") & ends_with("_answer")) %>%
-      pivot_longer(-user_id, names_to = "question", values_to = "answer", names_pattern = "(Q\\d+\\.\\d+)_answer")
-    
-    correctness <- df %>%
-      select(user_id = pin, starts_with("Q") & ends_with("_correct")) %>%
-      pivot_longer(-user_id, names_to = "question", values_to = "correct", names_pattern = "(Q\\d+\\.\\d+)_correct")
-    
-    points <- df %>%
-      select(user_id = pin, starts_with("Q") & ends_with("_points")) %>%
-      pivot_longer(-user_id, names_to = "question", values_to = "points", names_pattern = "(Q\\d+\\.\\d+)_points")
-    
-    # Combine reshaped data
-    data_long <- full_join(problems, answers, by = c("user_id", "question")) %>%
-      full_join(correctness, by = c("user_id", "question")) %>%
-      full_join(points, by = c("user_id", "question"))
-    
-    # Convert correct column to logical and points to integer
-    data_long$correct <- as.logical(data_long$correct)
-    data_long$points <- as.numeric(data_long$points)
-    
-    # Filter out first correct attempts
-    data_long <- data_long %>%
-      group_by(user_id, str_extract(question, "^Q\\d+")) %>%
-      arrange(as.numeric(str_extract(question, "\\d+$"))) %>%
-      filter(if (any(correct == TRUE)) correct == TRUE else TRUE) %>%
-      slice_min(order_by = as.numeric(str_extract(question, "\\d+$")))
-    
-    # Aggregate data by user_id and correct
-    user_data <- data_long %>%
-      group_by(user_id, correct) %>%
-      summarise(n = n(), .groups = 'drop')
-    
-    # Aggregate data by question and correct
-    question_data <- data_long %>%
-      group_by(question = str_extract(question, "^Q\\d+"), correct) %>%
-      summarise(n = n(), .groups = 'drop')
-    
-    # Calculate total points earned by each student
-    total_points_earned <- data_long %>%
-      filter(correct == TRUE) %>%
-      group_by(user_id) %>%
-      summarise(total_earned = sum(points, na.rm = TRUE))
-    
-    # Calculate total possible points for the assignment per student and find the maximum
-    total_points_possible <- data_long %>%
-      group_by(user_id) %>%
-      summarise(total_possible = sum(points, na.rm = TRUE)) %>%
-      summarise(max_total_possible = max(total_possible, na.rm = TRUE))
-    
-    # Assign the maximum total_possible to each student
-    total_points_possible_per_student <- data.frame(user_id = unique(data_long$user_id), total_possible = total_points_possible$max_total_possible)
-    
-    # Join total_points_earned and total_points_possible_per_student
-    grading <- full_join(total_points_earned, total_points_possible_per_student, by = "user_id")
-    
-    # Calculate percentage of points earned
-    grading <- grading %>%
-      mutate(percentage = (total_earned / total_possible) * 100)
-    
-    grading <- grading %>%
-      mutate(letter_grade = case_when(
-        percentage >= 93 ~ "A",
-        percentage >= 90 ~ "A-",
-        percentage >= 87 ~ "B+",
-        percentage >= 83 ~ "B",
-        percentage >= 80 ~ "B-",
-        percentage >= 77 ~ "C+",
-        percentage >= 73 ~ "C",
-        percentage >= 70 ~ "C-",
-        percentage >= 67 ~ "D+",
-        percentage >= 63 ~ "D",
-        percentage >= 60 ~ "D-",
-        TRUE ~ "F"
-      ))
-    
-    # Sort by percentage, highest to lowest
-    grading <- grading %>%
-      arrange(desc(percentage))
-    
-    # Calculate the maximum attempt number for each user and question, then average by question
-    question_attempt_data <- data_long %>%
-      mutate(attempt_number = as.numeric(str_extract(question, "\\d+$"))) %>%
-      group_by(user_id, question = str_extract(question, "^Q\\d+")) %>%
-      summarise(max_attempt = max(attempt_number)) %>%
-      group_by(question) %>%
-      summarise(avg_attempts = mean(max_attempt))
-    
-    # Calculate the maximum attempt number for each user and question, then average by user
-    user_attempt_data <- data_long %>%
-      mutate(attempt_number = as.numeric(str_extract(question, "\\d+$"))) %>%
-      group_by(user_id, question = str_extract(question, "^Q\\d+")) %>%
-      summarise(max_attempt = max(attempt_number)) %>%
-      group_by(user_id) %>%
-      summarise(avg_attempts = mean(max_attempt))
-    
-    return(list(user_data = user_data, question_data = question_data, grading = grading, question_attempt_data = question_attempt_data, user_attempt_data = user_attempt_data))
-  })
-  
-  output$data <- renderTable({
+  output$grades <- renderTable({
     req(data())
     data()
   })
   
-  output$grades <- renderTable({
-    req(analysis())
-    analysis()$grading
-  })
-  
-  # To add: plot rendering.
 }
 
 shinyApp(ui = ui, server = server)
